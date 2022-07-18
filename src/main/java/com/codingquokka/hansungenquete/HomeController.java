ec77ecb612c00a5c9f79d6e168e8af792e32fbe3
zack8361 <71763096+zack8361@users.noreply.github.com>
2022-07-18 15:12:19 +0900

    초기세팅
    초기

--- src/main/java/com/codingquokka/hansungenquete/HomeController.java
+++ src/main/java/com/codingquokka/hansungenquete/HomeController.java
@@ -23,9 +23,6 @@ public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
 	
 	
 	@RequestMapping(value = "/home", method = RequestMethod.GET)
