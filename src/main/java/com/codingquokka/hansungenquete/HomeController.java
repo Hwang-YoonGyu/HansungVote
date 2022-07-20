b91a1f65416a23895d8da58f3eeaca2e9fddd681
Hwang-YoonGyu <dotheji11@naver.com>
2022-07-20 13:27:17 +0900

    vote->Candidate

--- src/main/java/com/codingquokka/hansungenquete/HomeController.java
+++ src/main/java/com/codingquokka/hansungenquete/HomeController.java
@@ -1,17 +1,26 @@
 package com.codingquokka.hansungenquete;
 
+import java.io.IOException;
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 
 
--- src/main/java/domain/CandidateVO.java
+++ src/main/java/domain/CandidateVO.java
@@ -1,16 +1,16 @@
 package domain;
-public class VoteVO {
+public class CandidateVO {
 	   
-    private String vote_name;
+    private String candidate_name;
     private String election_name;
     private Byte[] image;
     private Integer vote_count;
    
-    public String getVote_Name() {
-        return vote_name;
+    public String getCandidate_name() {
+        return candidate_name;
     }
     public void setVote_name(String vote_name) {
-        this.vote_name = vote_name;
+        this.candidate_name = vote_name;
     }
     public String getElection_Name() {
         return election_name;
