935568af65a0d9a40a2dcaa58ac77e442af517af
SU_VIN <67284987+SU-VIN@users.noreply.github.com>
2022-10-08 16:26:34 +0900

    AES256.js

--- src/main/webapp/resources/js/encryption.js
+++ src/main/webapp/resources/js/encryption.js
@@ -1,18 +1,37 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.2/rollups/aes.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.2/rollups/sha256.js">/</script>
-// var message = "Led Zeppelin- Stairway to Heaven";
-// var passphrase = "1234";
-// var encrypt = CryptoJS.AES.encrypt(message, passphrase);
-// var decrypted = CryptoJS.AES.decrypt(encrypt, passphrase);
+var CryptoJS = require("crypto-js");
 
-function a()
-{
-    var message = "Led Zeppelin- Stairway to Heaven";
-    var passphrase = "1234";
-    var encrypt = CryptoJS.AES.encrypt(message, passphrase);
-    var decrypted = CryptoJS.AES.decrypt(encrypt, passphrase);
-    console.log(message,encrypt,decrypted);
-    alert("ㅎㅇ");
+const key ="ghkddbsrbqkrtjdwodlcksghdlatnqls";
+const Iv ="gkstjdqhxmwkdwkd"
+
+
+function aes256Encode(data){
+    const key ="ghkddbsrbqkrtjdwodlcksghdlatnqls";
+    const Iv ="gkstjdqhxmwkdwkd"
+    // [aes 인코딩 수행 실시 : cbc 모드]
+    const cipher = CryptoJS.AES.encrypt(data, CryptoJS.enc.Utf8.parse(key), {
+        iv: CryptoJS.enc.Utf8.parse(Iv), // [Enter IV (Optional) 지정 방식]
+        padding: CryptoJS.pad.Pkcs7,
+        mode: CryptoJS.mode.CBC // [cbc 모드 선택]
+    });
+
+    // [인코딩 된 데이터 확인 실시]
+    aes256EncodeData = cipher.toString();
+    return aes256EncodeData;
 }
 
 
+function aes256Decode(data){
+    const key ="ghkddbsrbqkrtjdwodlcksghdlatnqls";
+    const Iv ="gkstjdqhxmwkdwkd"
+
+    // [aes 디코딩 수행 실시 : cbc 모드]
+    const cipher = CryptoJS.AES.decrypt(data, CryptoJS.enc.Utf8.parse(key), {
+        iv: CryptoJS.enc.Utf8.parse(Iv), // [Enter IV (Optional) 지정 방식]
+        padding: CryptoJS.pad.Pkcs7,
+        mode: CryptoJS.mode.CBC // [cbc 모드 선택]
+    });
+
+    // [인코딩 된 데이터 확인 실시]
+    aes256DecodeData = cipher.toString(CryptoJS.enc.Utf8);
+    return aes256DecodeData
+}
